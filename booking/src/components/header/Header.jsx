bf8596b21c337476abe36b0229deb0a204d4f443
samade747 <samad.e747@gmail.com>
2024-04-23 12:50:12 +0500

    1249  23 4 24

---
+++ booking/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./navbar.css"
+
+const Header = () => {
+    return (
+     <div className="header">
+        <div className="headerList">    
+         <div className="headerListitem">
+             
+         </div>
+        </div>                      
+     </div>
+    )
+
+
+
+}
+
+export default Header
