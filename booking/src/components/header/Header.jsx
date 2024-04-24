0815d7e675ba0df67ce7825feec15edc419e3522
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-04-25 01:00:35 +0500

    Update Header.jsx

--- booking/src/components/header/Header.jsx
+++ booking/src/components/header/Header.jsx
@@ -5,8 +5,41 @@ const Header = () => {
      <div className="header">
         <div className="headerList">    
          <div className="headerListitem">
-         <FontAwesomeIcon icon="fa-solid fa-bed" />  
+         <FontAwesomeIcon icon={faBed} />  
+         <span>Hotels</span>
+          
+         <div className="headerListitem">
+         <FontAwesomeIcon icon={faPlane} />  
+         <span>Stays</span>
          </div>
+          
+         <div className="headerListitem">
+         <FontAwesomeIcon icon={faCar} />  
+         <span>flights</span>
+         </div>
+
+          
+         <div className="headerListitem">
+         <FontAwesomeIcon icon={faBed} />  
+         <span>Attractions </span>
+
+          
+         <div className="headerListitem">
+         <FontAwesomeIcon icon={faTaxi} />  
+         <span>Airport Hotel</span>
+         </div>
+
+         </div>
+
+
+
+
+
+
+
+
+
+
         </div>                      
      </div>
     )
